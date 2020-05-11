0f729755be4969a31b46b2c372b42ca6034f5570
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2020-05-11 14:09:37 -0400

    Updated

--- modules/linuxvmspoke/output.tf
+++
@@ -1,9 +0,0 @@
-output "vmnicip" {
-  description = "the ip address of the vm"  
-  value = azurerm_network_interface.myterraformnic.private_ip_address
-}
-
-output "id" {
-  description = "id of the provisioned vm"
-  value       = azurerm_virtual_machine.myterraformvm.id
-}
